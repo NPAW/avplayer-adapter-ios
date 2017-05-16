656fa94cc5a1115a7a68ee209603b42fab67106e
Joan Treveset <jtreveset@nicepeopleatwork.com>
2017-05-16 18:26:57 +0200

    Travis config

---
+++ scripts/build.sh
@@ -0,0 +1,34 @@
+
+# Development
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in scripts/certs/development-cert.cer.enc -d -a -out scripts/certs/development-cert.cer
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in scripts/certs/development-key.p12.enc -d -a -out scripts/certs/development-key.p12
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in scripts/provisioning-profile/development-provisioning-profile.mobileprovision.enc -d -a -out scripts/provisioning-profile/development-provisioning-profile.mobileprovision
+
+# Distribution
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in scripts/certs/distribution-cert.cer.enc -d -a -out scripts/certs/distribution-cert.cer
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in scripts/certs/distribution-key.p12.enc -d -a -out scripts/certs/distribution-key.p12
+
+# Create custom keychain
+security create-keychain -p $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain
+# Make the ios-build.keychain default, so xcodebuild will use it
+security default-keychain -s ios-build.keychain
+# Unlock the keychain
+security unlock-keychain -p $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain
+# Set keychain timeout to 1 hour for long builds
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+
+# Import certificates and keys
+security import ./scripts/certs/AppleWWDRCA.cer -k ios-build.keychain -A
+security import ./scripts/certs/development-cert.cer -k ios-build.keychain -A
+security import ./scripts/certs/development-key.p12 -k ios-build.keychain -P $SECURITY_PASSWORD -A
+security import ./scripts/certs/distribution-cert.cer -k ios-build.keychain -A
+security import ./scripts/certs/distribution-key.p12 -k ios-build.keychain -P $SECURITY_PASSWORD -A
+
+# Fix for OS X Sierra that hungs in the codesign step
+security set-key-partition-list -S apple-tool:,apple: -s -k $SECURITY_PASSWORD ios-build.keychain > /dev/null
+
+# Archive
+xcodebuild archive -workspace YouboraAVPlayerAdapter.xcworkspace -scheme AVPlayerAdapterExample -configuration Release -derivedDataPath ./build -archivePath ./build/Products/AVPlayerAdapterExample.xcarchive
+
+# Export IPA
+xcodebuild -exportArchive -archivePath ./build/Products/AVPlayerAdapterExample.xcarchive -exportOptionsPlist ./scripts/exportOptions-adHoc.plist -exportPath ./build/Products/IPA
