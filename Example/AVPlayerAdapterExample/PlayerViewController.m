76651a84a8dd40479bd3a45262db0d512f4ce45d
Enrique Alfonso Burillo <enrique@nicepeopleatwork.com>
2018-01-31 13:11:50 +0100

    Small change example

--- Example/AVPlayerAdapterExample/PlayerViewController.m
+++ Example/AVPlayerAdapterExample/PlayerViewController.m
@@ -65,7 +65,7 @@
 - (void) startYoubora {
     YBAVPlayerAdapter * adapter = [[YBAVPlayerAdapter alloc] initWithPlayer:self.playerViewController.player];
     //Uncomment this if you don't want to create a new view every time playerItem is changed
-    adapter.supportPlaylists = NO;
+    //adapter.supportPlaylists = NO;
     [self.youboraPlugin setAdapter:adapter];
 }
 
