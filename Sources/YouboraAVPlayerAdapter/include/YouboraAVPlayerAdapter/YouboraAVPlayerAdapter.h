2f1ffb1aa80ff5a6773de77e460bf41521e1eea9
Elisabet Massó <elisabet.masso@nicepeopleatwork.com>
2023-05-18 15:04:10 +0200

    added module.map file and updated xcodeproj options

---
+++ Sources/YouboraAVPlayerAdapter/include/YouboraAVPlayerAdapter/YouboraAVPlayerAdapter.h
@@ -0,0 +1,16 @@
+//
+//  YouboraAVPlayerAdapter.h
+//  YouboraAVPlayerAdapter
+//
+//  Created by Elisabet Massó on 24/4/23.
+//  Copyright © 2023 NPAW. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include "YBAVPlayerAdapter.h"
+#include "YBAVPlayerAdsAdapter.h"
+#include "YBAVPlayerAdapterTranformer.h"
+
+#endif /* Header_h */
